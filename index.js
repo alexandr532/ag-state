a2a476e7808bd772ff9dc8e57b69f714b890ef20
Alexandr <alexandr532@proton.me>
2022-08-16 08:41:28 +0200

    Added basic package structure

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Alexandr <alexandr532@proton.me>.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree
+ *
+ * @AGState 2022-08-16
+ */
+export * from './lib/AGState';
